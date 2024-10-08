a69749d3f61082ecb615d5b6cba35b4d043e7aa6
renata-and <renata.androsova@gmail.com>
2024-10-08 19:03:43 +0700

    String to alternating case

--- 8kuy/is-the-string-uppercase.js
+++ 8kuy/is-the-string-uppercase.js
@@ -4,11 +4,25 @@
 
 // ☑️ SOLUTION
 
-String.prototype.isUpperCase = function () {
-  return this.toUpperCase() === this.toString();
+// String.prototype.isUpperCase = function () {
+//   return this.toUpperCase() === this.toString();
+// };
+
+// '#LOVEWins'.isUpperCase(); //false, '#LOVEWins is not upper case'
+// '#LoveWINS'.isUpperCase(); //false, '#LoveWINS is not upper case'
+// '#LOVEWINs'.isUpperCase(); //false, '#LOVEWINs is not upper case'
+// '#lOVEWINS'.isUpperCase(); //false, '#lOVEWINS is not upper case'
+
+String.prototype.toAlternatingCase = function () {
+  let str = '';
+  for (let i = 0; i < this.length; i++) {
+    if (this[i] === this[i].toLowerCase()) {
+      str += this[i].toUpperCase();
+    } else {
+      str += this[i].toLowerCase();
+    }
+  }
+  return str;
 };
 
-'#LOVEWins'.isUpperCase(); //false, '#LOVEWins is not upper case'
-'#LoveWINS'.isUpperCase(); //false, '#LoveWINS is not upper case'
-'#LOVEWINs'.isUpperCase(); //false, '#LOVEWINs is not upper case'
-'#lOVEWINS'.isUpperCase(); //false, '#lOVEWINS is not upper case'
+console.log('heLLo world'.toAlternatingCase()); // "HELLO WORLD"
